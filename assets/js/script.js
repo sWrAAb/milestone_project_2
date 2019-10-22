0f67216f082de3a3fa62637e29712f76114fbbac
sWrAAb <swraab@gmail.com>
2019-10-22 20:09:54 +0000

    added resetBoard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,7 @@ let firstCard, secondCard;
 
 function flipCard() {
     if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.add("flip");
 
     if (!hasFlippedCard) {
@@ -47,9 +48,7 @@ function flipCard() {
         firstCard = this;
         return;
     }
-    hasFlippedCard = false;
     secondCard = this;
-
     checkForMatch();
 }
 
@@ -61,6 +60,7 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
+    resetBoard();
 }
 
 function unflipCards() {
@@ -68,10 +68,14 @@ function unflipCards() {
     setTimeout(() => {
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
-        lockBoard = false;
+        resetBoard();
     }, 1500);
 }
 
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
 
 cards.forEach(card => card.addEventListener("click", flipCard));
 
